9cb12f34e7ec79048db5147d50eb0e621178a0bf
San Lam <san.lam@customink.com>
2016-10-31 14:53:16 -0400

    adjusted notes

--- ui.R
+++ ui.R
@@ -4,10 +4,8 @@ fluidPage(
   # Application title
   titlePanel("Summarized R Datasets"),
   
-  # Sidebar with controls to provide a caption, select a dataset,
-  # and specify the number of observations to view. Note that
-  # changes made to the caption in the textInput control are
-  # updated in the output area immediately as you type
+  # Sidebar with controls to select a dataset,
+  # and specify the number of observations to view. 
   sidebarLayout(
     sidebarPanel(
           selectInput("dataset", "Choose a dataset:", 
@@ -17,7 +15,7 @@ fluidPage(
     ),
     
     
-    # Show the caption, a summary of the dataset and an HTML 
+    # Show a summary of the dataset and an HTML 
     # table with the requested number of observations
     mainPanel(
 
